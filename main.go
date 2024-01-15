e24c39b26e66508ee7ced616a395edf316c9f708
vkudr <themoonissilent@gmail.com>
2024-01-15 08:31:46 +0300

    add simple template for home

--- main.go
+++ main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 
 	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintln(w, "<h1>Home page</h1>")
+
+	tmpl, err := template.ParseFiles("templates/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func contactsHandler(w http.ResponseWriter, r *http.Request) {
@@ -17,10 +26,22 @@ func contactsHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "<h1>Contacts page</h1>")
 }
 
+func userHandler(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Println("id = ", id)
+	fmt.Fprintf(w, "<p>Id: %s", id)
+}
+
 func main() {
 	r := chi.NewRouter()
 	r.Get("/", homeHandler)
 	r.Get("/contacts", contactsHandler)
+	r.Route("/user", func(r chi.Router) {
+		r.Route("/{id}", func(r chi.Router) {
+			r.Get("/", userHandler)
+		})
+	})
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
