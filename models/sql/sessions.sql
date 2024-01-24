a5fab086aad5c4a448206c33da41d08c75f109ce
vkudr <themoonissilent@gmail.com>
2024-01-24 07:51:23 +0300

    update sessions table creation script

--- models/sql/sessions.sql
+++ models/sql/sessions.sql
@@ -1,5 +1,5 @@
 CREATE TABLE sessions (
     id SERIAL PRIMARY KEY,
-    user_id INT UNIQUE NOT NULL,
+    user_id INT UNIQUE REFERENCES users (id) ON DELETE CASCADE,
     token_hash TEXT NOT NULL UNIQUE
 );
