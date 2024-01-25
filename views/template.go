dd05372efd692195c2d460ac123ac3c729017afa
vkudr <themoonissilent@gmail.com>
2024-01-25 11:18:53 +0300

    fix sign out button visibility when logged in/logged out

--- views/template.go
+++ views/template.go
@@ -8,6 +8,8 @@ import (
 	"net/http"
 
 	"github.com/gorilla/csrf"
+	"github.com/ivykus/gallery/context"
+	"github.com/ivykus/gallery/models"
 )
 
 func Must(t Template, err error) Template {
@@ -24,8 +26,11 @@ type Template struct {
 func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
 	tpl := template.New(patterns[0])
 	tpl = tpl.Funcs(template.FuncMap{
-		"csrfField": func() template.HTML {
-			return `<input type="hidden" name="csrf" />`
+		"csrfField": func() (template.HTML, error) {
+			return "", fmt.Errorf("csrfField not implemented")
+		},
+		"currentUser": func() (template.HTML, error) {
+			return "", fmt.Errorf("currentUser not implemented")
 		},
 	})
 	tmpl, err := tpl.ParseFS(fs, patterns...)
@@ -59,6 +64,9 @@ func (t Template) Execute(w http.ResponseWriter, r *http.Request, data any) {
 	tpl = tpl.Funcs(template.FuncMap{
 		"csrfField": func() template.HTML {
 			return csrf.TemplateField(r)
+		},
+		"currentUser": func() *models.User {
+			return context.User(r.Context())
 		},
 	})
 
