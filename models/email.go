68995913a8ccf5be2335a94e22a14c45c1e04257
vkudr <themoonissilent@gmail.com>
2024-01-26 09:13:28 +0300

    create Email type and add SendEmail method for EmailService

--- models/email.go
+++ models/email.go
@@ -1,10 +1,22 @@
 package models
 
-import "github.com/go-mail/mail/v2"
+import (
+	"fmt"
+
+	"github.com/go-mail/mail/v2"
+)
 
 const (
 	DefaultSender = "support@ivygallery.com"
 )
+
+type Email struct {
+	From      string
+	To        string
+	Subject   string
+	Plaintext string
+	HTML      string
+}
 
 type SMTPConfig struct {
 	Host     string
@@ -25,4 +37,40 @@ type EmailService struct {
 	DefaultSender string
 
 	dialer *mail.Dialer
+}
+
+func (es *EmailService) SendEmail(email Email) error {
+	msg := mail.NewMessage()
+	es.setFrom(msg, email)
+	msg.SetHeader("To", email.To)
+	msg.SetHeader("Subject", email.Subject)
+	switch {
+	case email.Plaintext != "" && email.HTML != "":
+		msg.SetBody("text/plain", email.Plaintext)
+		msg.AddAlternative("text/html", email.HTML)
+	case email.Plaintext != "":
+		msg.SetBody("text/plain", email.Plaintext)
+	case email.HTML != "":
+		msg.AddAlternative("text/html", email.HTML)
+	}
+
+	err := es.dialer.DialAndSend(msg)
+	if err != nil {
+		return fmt.Errorf("send: %w", err)
+	}
+	return nil
+
+}
+
+func (es *EmailService) setFrom(msg *mail.Message, email Email) {
+	var from string
+	switch {
+	case email.From != "":
+		from = email.From
+	case es.DefaultSender != "":
+		from = es.DefaultSender
+	default:
+		from = DefaultSender
+	}
+	msg.SetHeader("From", from)
 }
