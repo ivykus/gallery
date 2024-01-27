c42142de34b2225e9083db711e5d31bee50f52c6
vkudr <themoonissilent@gmail.com>
2024-01-27 11:14:00 +0300

    implement Create method for PasswordResetService

--- models/password_reset.go
+++ models/password_reset.go
@@ -1,9 +1,14 @@
 package models
 
 import (
+	"crypto/sha256"
 	"database/sql"
+	"encoding/base64"
 	"fmt"
+	"strings"
 	"time"
+
+	"github.com/ivykus/gallery/rand"
 )
 
 const (
@@ -26,9 +31,63 @@ type PasswordResetService struct {
 }
 
 func (ps *PasswordResetService) Create(email string) (*PasswordReset, error) {
-	return nil, fmt.Errorf("TODO: implement PasswordResetService.Create")
+	email = strings.ToLower(email)
+	var userID int
+
+	row := ps.DB.QueryRow(`
+		SELECT id
+		FROM users
+		WHERE email = $1
+	`, email)
+	err := row.Scan(&userID)
+	if err != nil {
+		// TODO consider handling different types of errors for when the user
+		// is not found and if there is db error
+		return nil, fmt.Errorf("create: %w", err)
+	}
+
+	bytesPerToken := ps.BytesPerToken
+	if bytesPerToken < MinBytesPerToken {
+		bytesPerToken = MinBytesPerToken
+	}
+	token, err := rand.String(bytesPerToken)
+	if err != nil {
+		return nil, fmt.Errorf("create: %w", err)
+	}
+
+	duration := ps.Duration
+	if duration == 0 {
+		duration = DefaultResetDuration
+	}
+
+	pwReset := PasswordReset{
+		UserId:    userID,
+		Token:     token,
+		TokenHash: ps.hash(token),
+		ExpiresAt: time.Now().Add(duration),
+	}
+
+	row = ps.DB.QueryRow(`
+		INSERT INTO passwords_reset (user_id, token_hash, expires_at)
+		VALUES ($1, $2, $3)
+		ON CONFLICT (user_id) DO
+		UPDATE SET token_hash = $2, expires_at = $3
+		RETURNING id
+	`, pwReset.UserId, pwReset.TokenHash, pwReset.ExpiresAt)
+
+	err = row.Scan(&pwReset.ID)
+	if err != nil {
+		return nil, fmt.Errorf("create: %w", err)
+	}
+
+	return &pwReset, nil
 }
 
 func (ps *PasswordResetService) Consume(token string) (*User, error) {
 	return nil, fmt.Errorf("TODO: implement PasswordResetService.Consume")
+}
+
+func (ps *PasswordResetService) hash(token string) string {
+	hashedBytes := sha256.Sum256([]byte(token))
+	return base64.URLEncoding.EncodeToString(hashedBytes[:])
 }
