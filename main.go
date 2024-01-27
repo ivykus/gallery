108f04e300f8aaf89ec2c566e1e5b421eebad188
vkudr <themoonissilent@gmail.com>
2024-01-27 07:09:41 +0300

    add forgot-password template. Use it to set up routes.

--- main.go
+++ main.go
@@ -52,6 +52,8 @@ func main() {
 		"signup.gohtml", "tailwind.gohtml"))
 	usersC.Templates.SignIn = views.Must(views.ParseFS(templates.FS,
 		"signin.gohtml", "tailwind.gohtml"))
+	usersC.Templates.ForgotPassword = views.Must(views.ParseFS(templates.FS,
+		"forgot-pw.gohtml", "tailwind.gohtml"))
 
 	// setup routes
 	r := chi.NewRouter()
@@ -72,6 +74,8 @@ func main() {
 	r.Get("/signin", usersC.SignIn)
 	r.Post("/signin", usersC.ProcessSignIn)
 	r.Post("/signout", usersC.ProcessSignOut)
+	r.Get("/forgot-pw", usersC.ForgotPassword)
+	r.Post("/forgot-pw", usersC.ProcessForgotPassword)
 	r.Route("/users/me", func(r chi.Router) {
 		r.Use(umw.RequireUser)
 		r.Get("/", usersC.CurrentUser)
