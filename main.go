824c51beb64f241fb7be55aa833d40af24ddb713
vkudr <themoonissilent@gmail.com>
2024-01-27 11:10:04 +0300

    add routes for rese-pw pages

--- main.go
+++ main.go
@@ -101,6 +101,8 @@ func main() {
 		"forgot-pw.gohtml", "tailwind.gohtml"))
 	usersC.Templates.CheckYourEmail = views.Must(views.ParseFS(templates.FS,
 		"check-your-email.gohtml", "tailwind.gohtml"))
+	usersC.Templates.ResetPassword = views.Must(views.ParseFS(templates.FS,
+		"reset-pw.gohtml", "tailwind.gohtml"))
 
 	// setup routes
 	r := chi.NewRouter()
@@ -123,6 +125,8 @@ func main() {
 	r.Post("/signout", usersC.ProcessSignOut)
 	r.Get("/forgot-pw", usersC.ForgotPassword)
 	r.Post("/forgot-pw", usersC.ProcessForgotPassword)
+	r.Get("/reset-pw", usersC.ResetPassword)
+	r.Post("/reset-pw", usersC.ProcessResetPassword)
 	r.Route("/users/me", func(r chi.Router) {
 		r.Use(umw.RequireUser)
 		r.Get("/", usersC.CurrentUser)
