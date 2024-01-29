0d4a48647b736cead45da560e89211a65d95ae39
vkudr <themoonissilent@gmail.com>
2024-01-29 10:06:01 +0300

    add edit gallery handler for gallery controller

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -3,8 +3,11 @@ package controllers
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/ivykus/gallery/context"
+	"github.com/ivykus/gallery/errors"
 	"github.com/ivykus/gallery/models"
 )
 
@@ -36,4 +39,23 @@ func (g Gallery) Create(w http.ResponseWriter, r *http.Request) {
 	}
 	editURL := fmt.Sprintf("/galleries/%d/edit", gallery.ID)
 	http.Redirect(w, r, editURL, http.StatusFound)
+}
+
+func (g Gallery) Edit(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		http.Error(w, "Invalid gallery ID", http.StatusNotFound)
+		return
+	}
+	gallery, err := g.GalleryService.ByID(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNotFound) {
+			http.Error(w, "Gallery not found", http.StatusNotFound)
+			return
+		}
+		fmt.Println("gallery edit", err.Error())
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		return
+	}
+	g.Template.New.Execute(w, r, gallery)
 }
