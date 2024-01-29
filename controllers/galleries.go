56623012614ae612ced706674421e1e7ba862d12
vkudr <themoonissilent@gmail.com>
2024-01-29 09:14:52 +0300

    add gallery create method and a post query for routes

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -1,8 +1,10 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/ivykus/gallery/context"
 	"github.com/ivykus/gallery/models"
 )
 
@@ -19,4 +21,19 @@ func (g Gallery) New(w http.ResponseWriter, r *http.Request) {
 	}
 	data.Title = r.FormValue("title")
 	g.Template.New.Execute(w, r, data)
+}
+
+func (g Gallery) Create(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		Title string
+	}
+	data.Title = r.FormValue("title")
+	userId := context.User(r.Context()).Id
+	gallery, err := g.GalleryService.Create(data.Title, userId)
+	if err != nil {
+		g.Template.New.Execute(w, r, data)
+		return
+	}
+	editURL := fmt.Sprintf("/galleries/%d/edit", gallery.ID)
+	http.Redirect(w, r, editURL, http.StatusFound)
 }
